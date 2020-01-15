b7013f61f6de0cbc95e57051d1b8adfe0937212b
chen <chen_andrew@berkeley.edu>
2020-01-15 04:07:36 -0800

    Links and buttons now work

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+let resumeBtn = document.getElementById("btn-resume");
+resumeBtn.onclick = () => {
+    window.open("Andrew Chen - Resume.pdf", "_blank");
+} 
+/** Copy text on click */
+document.querySelectorAll(".click-copy").forEach((link) => {
+    //todo
+});
