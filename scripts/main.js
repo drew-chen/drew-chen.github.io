46ca4c3f90172be4da3111daf53627d0189e0017
chen <chen_andrew@berkeley.edu>
2020-01-18 04:14:09 -0800

    Start trying to blur project preview image

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,21 @@ let resumeBtn = document.getElementById("btn-resume");
 resumeBtn.onclick = () => {
     window.open("Andrew Chen - Resume.pdf", "_blank");
 } 
+
+function a(){
+if (document.body.classList.contains("modal-open")) {
+    document.body.classList.add("blur");
+} else {
+    document.body.classList.remove("blur");
+}
+}
+//todo
+/** Blur cards on hover. */
+// document.querySelectorAll(".card").forEach((card) => {
+//     card.onmouseover = () => card.classList.add("blur");
+//     card.onmouseleave = () => card.classList.remove("blur");
+// });
+
 /** Copy text on click */
 document.querySelectorAll(".click-copy").forEach((link) => {
     //todo
