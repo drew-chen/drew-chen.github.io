32765fafa3f7fd06733de18de1d47a93b77512e1
chen <chen_andrew@berkeley.edu>
2020-01-20 02:10:56 -0800

    Create blur on open modal and close blur ONLY when pressing 'X'

--- scripts/main.js
+++ scripts/main.js
@@ -3,21 +3,38 @@ resumeBtn.onclick = () => {
     window.open("Andrew Chen - Resume.pdf", "_blank");
 } 
 
-function a(){
-if (document.body.classList.contains("modal-open")) {
-    document.body.classList.add("blur");
-} else {
-    document.body.classList.remove("blur");
+/**
+ * Waits until an element is loaded before calling a function on that element.
+ * @param {string} query The selector of the element.
+ * @param {*} callBack A single function to be called upon element loading.
+ */
+function callWhenLoaded(query, callBack){
+    window.setTimeout(function(){
+      let element = document.querySelector(query);
+      if (element) {
+        callBack(element);
+      } else {
+        callWhenLoaded(query);
+      }
+    }, 500)
 }
+
+let main = document.querySelector("main");
+
+function openModal () {
+    main.style.filter = "blur(2px)";
+    callWhenLoaded(".modal.show .close", backDrop => {
+        backDrop.onclick = () => {main.style.filter = "none"};
+    });
 }
-//todo
-/** Blur cards on hover. */
-// document.querySelectorAll(".card").forEach((card) => {
-//     card.onmouseover = () => card.classList.add("blur");
-//     card.onmouseleave = () => card.classList.remove("blur");
-// });
+
+document.querySelectorAll("#projects .card").forEach((projectBtn) => {
+    projectBtn.onclick = openModal;
+});
+
+
 
 /** Copy text on click */
-document.querySelectorAll(".click-copy").forEach((link) => {
-    //todo
-});
+// document.querySelectorAll(".click-copy").forEach((link) => {
+//     //todo
+// });
