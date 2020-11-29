2667d7f2e038de65d527474b831bbdfc136cebd5
Andrew Chen <andrewchen747@gmail.com>
2020-11-28 22:20:36 -0800

    Remove unneeded javascript for focusing and scrolling

--- scripts/main.js
+++ scripts/main.js
@@ -9,17 +9,19 @@ function callWhenLoaded(query, callBack){
       if (element) {
         callBack(element);
       } else {
-        callWhenLoaded(query);
+        callWhenLoaded(query, callBack);
       }
-    }, 50)
+    }, 100)
 }
 
 const mainElement = document.querySelector("main");
 
 /**
  * Adds background blur when modal is shown. This blur is removed when closed.
+ * @param {Object} projectBtn The dom object representing the project card button.
  */
-function addBackgroundBlur() {
+function addBackgroundBlur(projectBtn) {
+    console.log(projectBtn);
     mainElement.style.filter = "blur(2px)";
     /**
      * Callback removes background blur on click of the opened modal.
@@ -27,46 +29,14 @@ function addBackgroundBlur() {
      */
     callWhenLoaded(".modal.show", backdrop => {
         backdrop.addEventListener('click', () => {
-            mainElement.style.filter = "none"
-            document.activeElement.blur()
+            mainElement.style.filter = "none";
+            // projectBtn.blur();
         });
     });
 }
 
 document.querySelectorAll("#projects .card").forEach((projectBtn) => {
-    projectBtn.addEventListener('click', addBackgroundBlur);
+    projectBtn.addEventListener('click', 
+        addBackgroundBlur.bind(this, projectBtn)
+    );
 });
-
-/** 
- * Gets the hash parameter in the url on load. If the parameter matches a project
- * then the website scrolls to the projects section and focuses the matching
- * project.
- */
-function focusUrlHashProject () {
-    if (!window.location.hash) {
-        return
-    }
-    const hashUrl = window.location.hash.slice(1)
-    const projectDescriptionIds = new Set([
-        "social-network",
-        "relationaldb",
-        "sketch",
-        "git",
-        "this-website",
-        "cipher-machine"
-    ])
-    if (projectDescriptionIds.has(hashUrl)) {
-        const projectsSection = document.getElementById("projects")
-        projectsSection.scrollIntoView()
-        const project = document.getElementById(`${hashUrl}-card`)
-        project.focus()
-    }
-
-}
-
-// Focus a specific project when the website is loaded, or when the url changes.
-focusUrlHashProject()
-callWhenLoaded("#projects", () => {
-    window.addEventListener("hashchange", focusUrlHashProject)
-});
-
